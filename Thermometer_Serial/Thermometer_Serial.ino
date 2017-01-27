dd8bd1775095c75b8c29975c262c48bc7a50afd8
antikz23 <jslizzz30@gmail.com>
2017-01-27 13:54:12 -0500

    Add files via upload

---
+++ Thermometer_Serial/Thermometer_Serial.ino
@@ -0,0 +1,17 @@
+
+float x;
+
+void setup()
+{
+  pinMode(23, INPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  x = analogRead(0);
+  x = (x*5)/1024;
+ // analogWrite();
+  Serial.println(x);
+  delay(500);
+}
