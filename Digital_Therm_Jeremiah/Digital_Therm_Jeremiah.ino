e07a7e0845210235cc552308a33837693b880496
antikz23 <jslizzz30@gmail.com>
2017-01-30 13:09:44 -0500

    Add files via upload

---
+++ Digital_Therm_Jeremiah/Digital_Therm_Jeremiah.ino
@@ -0,0 +1,21 @@
+float x;
+float y;
+
+void setup()
+{
+  pinMode(23, INPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  x = analogRead(0);
+  x = (x*5)/1024*100;
+  y = analogRead(0);
+  y = (x*9/5)+32;
+ // analogWrite();
+  Serial.print(x);
+  Serial.print(", ");
+  Serial.println(y);
+  delay(1000);
+}
