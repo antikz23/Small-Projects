5e0a69eb677856791194472b0e2f98255890a2c1
antikz23 <jslizzz30@gmail.com>
2017-02-17 13:41:22 -0500

    Add files via upload

---
+++ Inputs_Review/Inputs_Review.ino
@@ -0,0 +1,22 @@
+int x;
+int y;
+void setup()
+{
+ pinMode(9, OUTPUT);
+ pinMode(11, INPUT);
+ pinMode(12, INPUT);
+}
+
+void loop()
+{
+  x = digitalRead(11);
+  y = digitalRead(12);
+ if (y == LOW)
+ {
+   digitalWrite(9, LOW);
+ }
+ else 
+ {
+   digitalWrite(9, HIGH);
+ }
+}
---
+++ Outputs_Review/Outputs_Review.ino
@@ -0,0 +1,28 @@
+int x;
+
+void setup()
+{
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  for(int x = 0; x < 4; x++)
+  {
+    digitalWrite(9, LOW);
+    digitalWrite(10, HIGH);
+    delay(175);
+    digitalWrite(9, HIGH);
+    digitalWrite(10, LOW);
+    delay(175);
+    digitalWrite(9, LOW);
+    digitalWrite(10, HIGH);
+    delay(175);
+    digitalWrite(9, HIGH);
+    digitalWrite(10, LOW);
+    delay(500);
+  }
+}
+
+void loop()
+{
+
+}
+
