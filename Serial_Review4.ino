9311b925417287265e42d86a378376600bc5f011
antikz23 <jslizzz30@gmail.com>
2017-03-06 14:10:10 -0500

    Add files via upload

---
+++ Serial_Review4.ino
@@ -0,0 +1,17 @@
+int x = 0;
+void setup()
+{
+ Serial.begin(9600);
+}
+
+void loop()
+{
+  Serial.print(x, DEC);
+  Serial.print(", ");
+  Serial.print(x, BIN);
+  Serial.print(", ");
+  Serial.write(x);
+  Serial.print(", ");
+  Serial.println(x++, HEX);
+  delay(500);
+}
