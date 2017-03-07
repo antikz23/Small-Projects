e386d68cfe9b1d8df92c2c2cebc38605690d785a
antikz23 <jslizzz30@gmail.com>
2017-03-07 13:58:32 -0500

    Add files via upload

---
+++ Variables_Review1.ino
@@ -0,0 +1,31 @@
+int x = 0;
+byte y = 0;
+unsigned int z = 0;
+long a = 0;
+unsigned long b = 0;
+float c = 0;
+double d = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  Serial.print(x--);
+  Serial.print(",  ");
+  Serial.print(y--);
+  Serial.print(",  ");
+  Serial.print(z--);
+  Serial.print(",  ");
+  Serial.print(a--);
+  Serial.print(",  ");
+  Serial.print(b--);
+  Serial.print(",  ");
+  Serial.print(c--);
+  Serial.print(",  ");
+  Serial.println(d--);
+  delay(10);
+}
+
