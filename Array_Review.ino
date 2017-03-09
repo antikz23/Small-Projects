c0ad9453d362022cf3f8960d8a06112500080274
antikz23 <jslizzz30@gmail.com>
2017-03-09 13:40:37 -0500

    Add files via upload

---
+++ Array_Review.ino
@@ -0,0 +1,21 @@
+int x;
+byte ar[5] = {8, 12, 32, 44, 19};
+byte gr[5];
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  for (x = 0; x < 5; x++)
+  {
+    gr[x]=ar[x] + 2;
+    Serial.print(ar[x]);
+    Serial.print(":");
+    Serial.print(gr[x]);
+    Serial.print("  ");
+    delay(500);
+  }
+  Serial.println(":");
+}
