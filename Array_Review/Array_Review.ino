02bbb4a8796efd7451ee556245c9bc6a1a7887f5
antikz23 <jslizzz30@gmail.com>
2017-03-10 14:11:59 -0500

    Add files via upload

---
+++ Array_Review/Array_Review.ino
@@ -0,0 +1,32 @@
+int state = 0;
+int x;
+void setup()
+{
+  pinMode(9, INPUT);
+  pinMode(10, OUTPUT);
+}
+
+void loop()
+{
+  x = digitalRead(9);
+  switch (state)
+  {
+    case 0:
+      if (x == LOW)
+      {
+        digitalWrite(10, LOW);
+        state = 1;
+      }
+      delay(500);
+      break;
+
+    case 1:
+      if (x == LOW)
+      {
+        digitalWrite(10, HIGH);
+        state = 0;
+      }
+      delay(500);
+      break;
+  }
+}
