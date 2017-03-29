a98de07b75026a9531c023bb6e96dd294148eaa6
antikz23 <jslizzz30@gmail.com>
2017-03-29 14:02:03 -0400

    Add files via upload

---
+++ Color_Sensor_Jeremiah.ino
@@ -0,0 +1,21 @@
+int x;
+void setup()
+{
+  pinMode(A0, INPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  x = analogRead(0);
+  digitalWrite(9, LOW);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, HIGH);
+  Serial.println(x);
+  delay(500);
+}
+
+
