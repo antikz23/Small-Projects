5b6c2ccef0734126a659a26cd36d18810a9c2935
antikz23 <jslizzz30@gmail.com>
2017-04-27 14:13:15 -0400

    Add files via upload

---
+++ Candy_Sorter_Servo_JB/Candy_Sorter_Servo_JB.ino
@@ -0,0 +1,25 @@
+#include <Servo.h>
+
+Servo myservo;
+
+void setup() 
+{
+  myservo.attach(9);
+}
+
+void loop() 
+{
+ myservo.write(30);
+ delay(300);
+ myservo.write(60);
+ delay(300);
+ myservo.write(90);
+ delay(300);
+ myservo.write(120);
+ delay(300);
+ myservo.write(150);
+ delay(300);
+ myservo.write(180);
+ delay(300);
+ myservo.write(-180);
+}
