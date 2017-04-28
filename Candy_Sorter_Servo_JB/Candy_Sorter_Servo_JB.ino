b0200d56b222914e42ca839bef6fca080e9a71de
antikz23 <jslizzz30@gmail.com>
2017-04-28 13:56:38 -0400

    Add files via upload

--- Candy_Sorter_Servo_JB/Candy_Sorter_Servo_JB.ino
+++ Candy_Sorter_Servo_JB/Candy_Sorter_Servo_JB.ino
@@ -4,22 +4,22 @@ Servo myservo;
 
 void setup() 
 {
-  myservo.attach(9);
+  myservo.attach(9, 300, 2700);
+// ././ myservo.setMaximumPulse(2500);
+// myservo.setMinimumPulse(500);
 }
 
 void loop() 
 {
+ myservo.write(0);
+ delay(1000);
  myservo.write(30);
- delay(300);
- myservo.write(60);
- delay(300);
- myservo.write(90);
- delay(300);
- myservo.write(120);
- delay(300);
- myservo.write(150);
- delay(300);
- myservo.write(180);
- delay(300);
- myservo.write(-180);
+ delay(1000);
+ 
+ myservo.write(70);
+ delay(1000);
+ myservo.write(110);
+ delay(1000);
+ myservo.write(170);
+ delay(1000);
 }
