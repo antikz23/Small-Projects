b8bc1b1a12dffc93d24542e089d0a08ecd9c4115
antikz23 <jslizzz30@gmail.com>
2017-05-01 13:21:13 -0400

    Add files via upload

--- Candy_Sorter_Servo_JB/Candy_Sorter_Servo_JB.ino
+++ Candy_Sorter_Servo_JB/Candy_Sorter_Servo_JB.ino
@@ -1,25 +1,42 @@
 #include <Servo.h>
 
 Servo myservo;
-
-void setup() 
+char x = 0;
+void setup()
 {
-  myservo.attach(9, 300, 2700);
-// ././ myservo.setMaximumPulse(2500);
-// myservo.setMinimumPulse(500);
+  Serial.begin(9600);
+  myservo.attach(9, 500, 2700);
+  // ././ myservo.setMaximumPulse(2500);
+  // myservo.setMinimumPulse(500);
 }
 
-void loop() 
+void loop()
 {
- myservo.write(0);
- delay(1000);
- myservo.write(30);
- delay(1000);
- 
- myservo.write(70);
- delay(1000);
- myservo.write(110);
- delay(1000);
- myservo.write(170);
- delay(1000);
+  if (Serial.available() > 0)
+  {
+    x = Serial.read();
+    Serial.println(x);
+
+    if (x == 'r')
+    {
+      myservo.write(170);
+     // x = 0;
+    }
+    if (x == 'o')
+    {
+      myservo.write(120);
+     // x = 0;
+    }
+  }
+
+  // myservo.write(0);
+  //delay(1000);
+  //myservo.write(30);
+  //delay(1000);
+  //myservo.write(70);
+  //delay(1000);
+  //myservo.write(120);
+  //delay(1000);
+  //myservo.write(170);
+  //delay(1000);
 }
