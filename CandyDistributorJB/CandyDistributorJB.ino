b410cafb3b9df781d40dfced979fe5dd82f835ad
antikz23 <jslizzz30@gmail.com>
2017-05-17 13:59:41 -0400

    Add files via upload

---
+++ CandyDistributorJB/CandyDistributorJB.ino
@@ -0,0 +1,20 @@
+#include <Servo.h>
+
+Servo myservo;
+int pos = 0;
+void setup()
+{
+  myservo.attach(9, 500, 2700);
+  // ././ myservo.setMaximumPulse(2500);
+  // myservo.setMinimumPulse(500);
+}
+
+void loop()
+{
+  myservo.write(45);
+  delay(3000);
+  myservo.write(90);
+  delay(3000);
+  myservo.write(127);
+  delay(3000);
+}
